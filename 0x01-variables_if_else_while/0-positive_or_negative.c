71d03f6d862ca649fcb1cfa2cc0edc404a75374a
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2022-12-11 12:24:30 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -18,6 +19,6 @@ int main(void)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-	
-	return (0);	
+
+	return (0);
 }
