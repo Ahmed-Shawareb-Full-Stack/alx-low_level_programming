12bdaf050ae5d1a01dfe97494ecae15ca8e6fafe
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2022-12-12 03:19:34 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	alpha = "a";
+	while (alpha <= "z")
+	{
+		putchar("%c \n", alpha);
+	}
+
+	return (0);
+}
