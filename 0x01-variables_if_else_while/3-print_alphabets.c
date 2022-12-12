ced4766d5320f6deb25811ce1cfe87766d36f2a3
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2022-12-12 06:26:27 -0800

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,15 @@ int main(void)
 	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{	
 		putchar(alpha);
 		putchar('\n');
+	}
 	for (alpha = 'A'; alpha <= 'Z'; alpha++)
-		putchar()alpha
+	{
+		putchar(alpha)
 		putchar('\n');
+	}
 
 	return (0);
 }
