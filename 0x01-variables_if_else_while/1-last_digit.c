5d0667d2d4d8944da728a8dd904ad1b6c44f1e87
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2022-12-12 03:10:26 -0800

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  *
- * Reuturn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 	int n;
 	int last_digit;
@@ -20,7 +20,7 @@ int main()
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	} else if (last_digit == 0)
 	{
-		printf("Last digit of %d is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	} else if (last_digit < 6 && last_digit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
