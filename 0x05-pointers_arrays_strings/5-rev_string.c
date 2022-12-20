615075341432865d265cb1bb6c3b356567abedb8
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2022-12-20 11:19:54 -0800

    0x04-pointers_arrays_strings task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<string.h>
+/**
+ * rev_string - reverse a string
+ *
+ * @s: input string
+ */
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	char tmp;
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
+}
