85864a9fbcb8f927215fcb65295f7ce791e42a63
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2022-12-20 11:53:09 -0800

    0x04-pointers_arrays_strings task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "mian.h"
+
+/**
+ * puts-half - print second have of a string
+ * @s: input string
+ */
+
+void puts_half(char *s)
+{
+	int i = 0, len = 0, j;
+
+	while (s[i++])
+		len++;
+
+	if ((len % 2) == 0)
+		j = len / 2;
+
+	else
+		j = (len + 1) / 2;
+
+	for (i = j; i < len; i++)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
