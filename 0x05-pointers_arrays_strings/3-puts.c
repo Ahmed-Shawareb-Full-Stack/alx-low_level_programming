0dcd320f4736e6198d84fa59f3f6472cb130f89e
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2022-12-20 09:01:13 -0800

    0x04-pointers_arrays_strings task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - print the input string followed by a new line
+ * @s: input string
+ * Return: no retur
+ */
+
+int _puts(*s)
+{
+	while (*s)
+		_putchar(*++s);
+	_putchar('\n');
+}
