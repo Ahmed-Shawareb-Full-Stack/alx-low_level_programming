a31918eb01bb67f3f77342ef768fb4902a160939
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2022-12-22 02:47:41 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int i = 0;
 
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		for (; i < n; i++)
+			dest[i] = '\0';
+	}
 	return (dest);
 }
