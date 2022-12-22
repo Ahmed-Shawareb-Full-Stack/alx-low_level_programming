176032f71b66fbbda86487883b3addbac48fb57d
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2022-12-22 02:56:30 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,26 +2,16 @@
 #include<string.h>
 
 /**
- * _strncpy - copy a text to a dest
+ * _strncpy - copy a text from src to a dest
  * @dest: text copy destination
  * @src: text to be copied
  * @n: limit of text
- * return: dest
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	strncpy(dest, src, n);
 
-	while (i < n && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	if (i < n)
-	{
-		for (; i < n; i++)
-			dest[i] = '\0';
-	}
 	return (dest);
 }
