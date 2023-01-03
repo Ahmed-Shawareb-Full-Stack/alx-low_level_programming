4fedd755d3a1b4a53f365e9afa684f56b02e58a1
Ahmed-Shawareb-Full-Stack <js.dev.ahmedShawareb@gmail.com>
2023-01-02 18:30:49 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _strchr - locates a character in a string .
  * @s: string to check
  * @c: character to found
- * UPDATE V1.0 : Features : Pointers implementation instead of Arrays .
  * Pointer/memory incrementation instead of i .
  * Return: pointer to s or null
  */
