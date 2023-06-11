1dadec1b1620b3cd057894dfa79df237bbeb4ca4
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2023-06-11 03:21:34 +0300

    0x0A-argc_argv: task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+/**
+ *main - printsthe program name
+ *@argc: the number of args that is passed to the program
+ *@argv: the array of pointers to the args
+ *
+ *return: 0
+ * */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n" , argv[0]);
+	
+	return (0);
+}
